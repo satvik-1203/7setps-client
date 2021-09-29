fb0dfda196bbd96f0b0bb48b9f74034dda2ac95a
satvik-1203 <satvikgsu@gmail.com>
2021-09-29 00:38:25 -0400

    :construction: working on sign up page

--- components/pages/SignUp/index.tsx
+++ components/pages/SignUp/index.tsx
@@ -48,21 +48,6 @@ const index: React.FC<Props> = () => {
           </div>
           <Button data={{ name: "Sign up", type: "submit" }} />
         </form>
-        <div className="relative">
-          <input
-            id="test"
-            name="test"
-            type="text"
-            className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-rose-600"
-            placeholder="john@doe.com"
-          />
-          <label
-            htmlFor="test"
-            className="absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-          >
-            Email address
-          </label>
-        </div>
       </main>
     </div>
   );
